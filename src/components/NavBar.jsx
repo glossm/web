2b608a001d81ed8a0564f4f395daa267f86806a8
KH Luke Kim <khlukekim@gmail.com>
2019-04-21 10:41:33 +0900

    fix/apply i18n to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Container, Dropdown, Menu, Image } from 'semantic-ui-react';
+import { useTranslation} from 'react-i18next';
 
 import images from '../assets/images';
 
@@ -16,6 +17,7 @@ const defaultProps = {
 };
 
 function NavBar(props) {
+  const { t, i18n} = useTranslation();
   const { user, onLogout, pathname } = props;
 
   const isActive = path => pathname.includes(path);
@@ -23,16 +25,16 @@ function NavBar(props) {
     <Menu.Menu position="right">
       <Dropdown item text={user.username}>
         <Dropdown.Menu>
-          <Dropdown.Item text="My Profile" className="NavBar-item"  as={Link} to="/profile/" />
-          <Dropdown.Item text="Logout" className="NavBar-item"  onClick={onLogout} />
+          <Dropdown.Item text={t('navbar.myprofile')} className="NavBar-item"  as={Link} to="/profile/" />
+          <Dropdown.Item text={t('navbar.logout')} className="NavBar-item"  onClick={onLogout} />
         </Dropdown.Menu>
       </Dropdown>
     </Menu.Menu>
   );
   const NotAuthenticatedMenu = () => (
     <Menu.Menu position="right">
-      <Menu.Item name="sign up" className="NavBar-item"  as={Link} to="/signup/" />
-      <Menu.Item name="login" className="NavBar-item"  as={Link} to="/login/" />
+      <Menu.Item name={t('navbar.signup')} className="NavBar-item"  as={Link} to="/signup/" />
+      <Menu.Item name={t('navbar.login')} className="NavBar-item"  as={Link} to="/login/" />
     </Menu.Menu>
   );
 
@@ -42,8 +44,8 @@ function NavBar(props) {
         <Menu.Item name="glossm" as={Link} to="/" header>
           <Image src={images.logoTransparent} size="small"/>
         </Menu.Item>
-        <Menu.Item name="learn" className="NavBar-item" as={Link} to="/learn/" active={isActive('learn/')} />
-        <Menu.Item name="language" className="NavBar-item" as={Link} to="/language/" active={isActive('language/')} />
+        <Menu.Item name={t('navbar.learn')} className="NavBar-item" as={Link} to="/learn/" active={isActive('learn/')} />
+        <Menu.Item name={t('navbar.language')} className="NavBar-item" as={Link} to="/language/" active={isActive('language/')} />
         {user ? <AuthenticatedMenu /> : <NotAuthenticatedMenu />}
       </Container>
     </Menu>
