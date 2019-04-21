463f37f6f6e11d47350862723410973e0a27297e
KH Luke Kim <khlukekim@gmail.com>
2019-04-21 10:40:31 +0900

    feat/ topic list style and availablity

--- src/pages/TopicList.jsx
+++ src/pages/TopicList.jsx
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button, Card, Container, Header, Progress } from 'semantic-ui-react';
+import { Button, Card, Container, Header, Progress, Segment } from 'semantic-ui-react';
 import axios from 'axios';
+import { noAuto } from '@fortawesome/fontawesome-svg-core';
 
 const propTypes = {
   match: PropTypes.shape({}).isRequired,
@@ -24,8 +25,18 @@ class TopicList extends Component {
 
   onGoBack = () => this.props.history.push('/learn/');
 
+  // unnecessary recursion
+  levelEnabled = (level) => {
+    const topicsByLevel = _.groupBy(this.state.topics, 'level');
+    const enabled =  (level == 1) ? true :
+      topicsByLevel[level-1].reduce((a, b) => {
+        return a && ( b.progress.current == b.progress.total );
+      }, this.levelEnabled(level-1));
+    return enabled;
+  }
+
   renderTopicCard = langId => (topic) => {
-    const { id: topicId, name, progress } = topic;
+    const { id: topicId, name, progress, level } = topic;
     const { current, total } = progress;
     const progressBar = (
       <Progress
@@ -36,13 +47,16 @@ class TopicList extends Component {
         success={current !== 0 && current === total}
       />
     );
+    const enabled = this.levelEnabled(level);
     return (
       <Card
+        style={{minWidth: '160px', backgroundColor: enabled?'white':'#cccccc'}}
         key={topicId.toString()}
-        as={Link}
+        as={enabled?Link:'div'}
         to={`/learn/${langId}/${topicId}/`}
         header={name}
         description={`${current} / ${total} learned`}
+        image={topic.image || '/topic_placeholder.png'}
         extra={progressBar}
       />
     );
@@ -58,14 +72,16 @@ class TopicList extends Component {
     return (
       <Container>
         <Header content="Select a Topic" size="large" className="top-header" />
+        <Segment style={{overflowX: 'scroll', overflowY: 'hidden', whiteSpace:'nowrap'}}>
         {sortedLevels.map(level => (
-          <div key={level} style={{ marginBottom: '2rem' }}>
+          <span key={level} style={{ marginBottom: '2rem', display: 'inline-block'}}>
             <Header content={`Level ${level}`} />
-            <Card.Group stackable itemsPerRow={4}>
+            <Card.Group stackable itemsPerRow={1}>
               {topicsByLevel[level].map(this.renderTopicCard(langId))}
             </Card.Group>
-          </div>
+          </span>
         ))}
+        </Segment>
         <Header size="tiny" />
         <Button content="Back to Languages" onClick={this.onGoBack} />
       </Container>
